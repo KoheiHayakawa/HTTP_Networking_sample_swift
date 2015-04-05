fd99d8618b9409b8f2bbbd93e712dd6eb434f22f
Kohei Hayakawa <hayakawa.kohei@itolab.nitech.ac.jp>
2015-04-05 19:16:09 +0900

    Add comment

--- HTTP_Networking_sample_ios_swift/AppDelegate.swift
+++ HTTP_Networking_sample_ios_swift/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        // setup root view
         let entryListTableViewController = EntryListTableViewController()
                 navigationController = UINavigationController(rootViewController: entryListTableViewController)
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
